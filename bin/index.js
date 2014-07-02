23a7cd1689a51d2ee9577af2b433a3c47a0ba285
jarvys <yuhan534@126.com>
2014-07-02 21:02:59 +0800

    * add alias for port * add -s option for daemon process

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,15 @@ var cli            = require('../src/cli')
 var notifier = updateNotifier({packagePath: '../package'})
 if (notifier.update) notifier.notify()
 
-var argv = minimist(process.argv.slice(2))
+var argv = minimist(process.argv.slice(2), {
+    boolean: ["silent"],
+    alias: {
+        'p': 'port',
+        's': 'silent'
+    },
+    default: {
+        silent: false
+    }
+})
 
 cli.run(argv)
