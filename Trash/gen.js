88e9c3c08004828a70014461acb0edd0651c9811
syzer3@gmail.com <syzer3@gmail.com>
2015-07-31 07:50:48 +0200

    removed dummy fixtures

--- Trash/gen.js
+++
@@ -1,4 +0,0 @@
-var other = require('./other/other')();
-module.exports = function () {
-    return { posts: [{id:2}], others: other.other }
-}
--- Trash/other/other.js
+++
@@ -1,9 +0,0 @@
-module.exports = function () {
-    return {
-        other: other()
-    }
-
-    function other() {
-        return [{id:9}]
-    }
-}
