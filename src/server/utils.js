ebe14808a6f33e7c24ca48b35e7559d066de1ae8
Simon Egersand <s.egersand@gmail.com>
2016-07-10 17:52:39 +0200

    Add support for big numbers

--- src/server/utils.js
+++ src/server/utils.js
@@ -17,7 +17,15 @@ function toNative (value) {
     } else if (value === 'true' || value === 'false') {
       return value === 'true'
     } else if (!isNaN(+value)) {
-      return +value
+      var numValue = Number.parseInt(value, 10)
+
+      // If the number is too large (not safe) for
+      // Javascript's Number object, return it as a string
+      if (!Number.isSafeInteger(numValue)) {
+        return '' + value
+      }
+
+      return numValue
     }
   }
   return value
