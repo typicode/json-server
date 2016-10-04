b070d5619435bf78d49fc883b58e72b1485d7ed4
Alex Ciobica <alex.ciobica@gmail.com>
2016-10-04 13:04:48 +0300

    Expose the Links header. (#381)

--- src/server/router/plural.js
+++ src/server/router/plural.js
@@ -153,7 +153,7 @@ module.exports = function (db, name) {
     // Slice result
     if (_end || _limit || _page) {
       res.setHeader('X-Total-Count', chain.size())
-      res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count')
+      res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count' + (_page ? ', Links' : ''))
     }
 
     if (_page) {
