1cd4afecb038aabcca4301130d4c613676e593a5
Nathan Trujillo <ntrujillo@illumina.com>
2017-10-11 19:00:05 -0700

    Fix tests (make eslint pretty)

--- test/server/singular.js
+++ test/server/singular.js
@@ -22,20 +22,31 @@ describe('Server', () => {
 
   describe('GET /:resource', () => {
     it('should respond with corresponding resource', () =>
-      request(server).get('/user').expect(db.user).expect(200))
+      request(server)
+        .get('/user')
+        .expect(db.user)
+        .expect(200))
   })
 
   describe('POST /:resource', () => {
     it('should create resource', () => {
       const user = { name: 'bar' }
-      return request(server).post('/user').send(user).expect(user).expect(201)
+      return request(server)
+        .post('/user')
+        .send(user)
+        .expect(user)
+        .expect(201)
     })
   })
 
   describe('PUT /:resource', () => {
     it('should update resource', () => {
       const user = { name: 'bar' }
-      return request(server).put('/user').send(user).expect(user).expect(200)
+      return request(server)
+        .put('/user')
+        .send(user)
+        .expect(user)
+        .expect(200)
     })
   })
 
