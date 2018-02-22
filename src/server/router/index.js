bed6d03d2798f429b044772a15f4b25809cd3fb9
ChristianAscone <ascone.christian@gmail.com>
2018-02-22 11:20:09 +0100

    Fix error message for both latest and 4 node versions

--- src/server/router/index.js
+++ src/server/router/index.js
@@ -65,10 +65,14 @@ module.exports = (source, opts = { foreignKeySuffix: 'Id' }) => {
         return
       }
 
+      var sourceMessage = ''
+      if (!_.isObject(source)) {
+        sourceMessage = `in ${source}`
+      }
+
       const msg =
-        `Type of "${key}" (${typeof value}) ${
-          _.isObject(source) ? '' : `in ${source}`
-        } is not supported. ` + `Use objects or arrays of objects.`
+        `Type of "${key}" (${typeof value}) ${sourceMessage} is not supported. ` +
+        `Use objects or arrays of objects.`
 
       throw new Error(msg)
     })
