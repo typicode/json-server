792b65662f845e41e633fca066d7cdff0329b9f3
typicode <typicode@gmail.com>
2018-05-28 21:39:21 +0200

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/front/index.js',
   module: {
     rules: [
-      { test: /\.css$/, use: 'css-loader' },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({ template: 'src/front/index.html' })
+    new HtmlWebpackPlugin({ template: 'src/front/index.html' }),
+    new MiniCssExtractPlugin()
   ]
 }
