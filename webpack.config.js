8c66a498cb046f70ddf1415df06d46eb0fccc15d
typicode <typicode@gmail.com>
2018-05-28 23:09:40 +0200

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
