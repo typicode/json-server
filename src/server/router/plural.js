eb075a3a060105bbf7110a9f3a0e01ca3185bb58
Hossain Khademian <hossain.khademian.1374@gmail.com>
2018-06-15 14:51:24 +0430

    include support for many to many
    first step to solve [this issue](https://github.com/typicode/json-server/issues/805)

--- src/server/router/plural.js
+++ src/server/router/plural.js
@@ -142,6 +142,8 @@ module.exports = (db, name, opts) => {
                 return value !== elementValue.toString()
               } else if (isLike) {
                 return new RegExp(value, 'i').test(elementValue.toString())
+              } else if(Array.isArray(elementValue)) {
+                return _.includes(_.map(elementValue, el => el.toString()), value)
               } else {
                 return value === elementValue.toString()
               }
