ec52f1ed1b294381c6485d6b47fe024f240696bf
typicode <typicode@gmail.com>
2019-09-02 17:58:21 +0200

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
