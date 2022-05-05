bc934fb8b22c36546cca81956edfd544e14f9892
Eric Newton <eric.newton@centralsquare.com>
2022-05-05 12:07:21 -0400

    reduced schema ignore tests (copied from other tests)

--- __tests__/server/schema-ignore.js
+++ __tests__/server/schema-ignore.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const jsonServer = require('../../src/server')
 
-describe('Schema-ignore', () => {
+describe('$schema-ignore', () => {
   let server
   let router
   let db
@@ -22,30 +22,7 @@ describe('Schema-ignore', () => {
     server.use(router)
   })
 
-  describe('GET /:resource', () => {
-    test('should respond with corresponding resource', () =>
-      request(server).get('/user').expect(200, db.user))
-  })
-
-  describe('POST /:resource', () => {
-    test('should create resource', () => {
-      const user = { name: 'bar' }
-      return request(server).post('/user').send(user).expect(201, user)
-    })
-  })
-
-  describe('PUT /:resource', () => {
-    test('should update resource', () => {
-      const user = { name: 'bar' }
-      return request(server).put('/user').send(user).expect(200, user)
-    })
-  })
-
-  describe('PATCH /:resource', () => {
-    test('should update resource', () =>
-      request(server)
-        .patch('/user')
-        .send({ name: 'bar' })
-        .expect(200, { name: 'bar', email: 'foo@example.com' }))
+  test('doesnt error with $schema node', () => {
+    return request(server).get('/user').expect(200, db.user)
   })
 })
