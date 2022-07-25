480776731e0e3a38f3d82dbbaa6b47b6e9692c78
wll8 <xwdaysme@gmail.com>
2022-07-25 09:59:43 +0800

    feat: export express

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 
 module.exports = {
+  lib: {
+    express,
+  },
   create: () => express().set('json spaces', 2),
   defaults: require('./defaults'),
   router: require('./router'),
