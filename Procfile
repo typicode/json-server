d5946c53220479faf77e9e408ffba6153f154cfe
Nik_Dov <kolyan25lk@gmail.com>
2022-10-09 03:26:57 +0300

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node server.js
---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults({
+  static: './build'
+});
+const PORT = process.env.PORT || 8000;
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}))
+server.use(router);
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
