e371afd9bb4f4313c3b7e04fa8eec0e7d8311bb8
ilyas akın <ashnwor@gmail.com>
2023-02-14 07:06:04 +0300

    Display type correctly for array if obj passed as an array (#1394)
    * Display type correctly for array if obj passed as an array
    
    * Narrow the test case for `should throw an error if data is an array`

--- src/server/router/validate-data.js
+++ src/server/router/validate-data.js
@@ -17,7 +17,7 @@ module.exports = (obj) => {
     Object.keys(obj).forEach(validateKey)
   } else {
     throw new Error(
-      `Data must be an object. Found ${typeof obj}.` +
+      `Data must be an object. Found ${Array.isArray(obj) ? 'array' : typeof obj}.` +
         'See https://github.com/typicode/json-server for example.'
     )
   }
