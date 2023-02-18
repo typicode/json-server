typicode/json-server

commit e371afd9bb4f4313c3b7e04fa8eec0e7d8311bb8

Display type correctly for array if obj passed as an array (#1394)
* Display type correctly for array if obj passed as an array

* Narrow the test case for `should throw an error if data is an array`

src/server/router/validate-data.js
@@ -17,7 +17,7 @@ module.exports = (obj) => {
     Object.keys(obj).forEach(validateKey)
   } else {
     throw new Error(
-      `Data must be an object. Found ${typeof obj}.` +
+      `Data must be an object. Found ${Array.isArray(obj) ? 'array' : typeof obj}.` +
         'See https://github.com/typicode/json-server for example.'
     )
   }

__tests__/server/validate-data.js
@@ -7,7 +7,7 @@ describe('validateData', () => {
   })
 
   test('should throw an error if data is an array', () => {
-    assert.throws(() => validateData([]), /must be an object/)
+    assert.throws(() => validateData([]), /Data must be an object. Found array./)
   })
 
   test("shouldn't throw an error", () => {

commit f457b098b1e951a15ba92e0719d53083c2a614b7

Fix CI failure caused by #1394 (#1406)

src/server/router/validate-data.js
@@ -17,8 +17,10 @@ module.exports = (obj) => {
     Object.keys(obj).forEach(validateKey)
   } else {
     throw new Error(
-      `Data must be an object. Found ${Array.isArray(obj) ? 'array' : typeof obj}.` +
-        'See https://github.com/typicode/json-server for example.'
+      `Data must be an object. Found ${
+        Array.isArray(obj) ? 'array' : typeof obj
+      }
+      'See https://github.com/typicode/json-server for example.`
     )
   }
 }

__tests__/server/validate-data.js
@@ -7,7 +7,7 @@ describe('validateData', () => {
   })
 
   test('should throw an error if data is an array', () => {
-    assert.throws(() => validateData([]), /Data must be an object. Found array./)
+    assert.throws(() => validateData([]), /Data must be an object. Found array/)
   })
 
   test("shouldn't throw an error", () => {