3e12cc1460eb1ea86d4e97c4a615c6cb3b70221a
typicode <typicode@gmail.com>
2023-02-22 22:37:47 +0100

    fix: typo

--- src/server/router/validate-data.js
+++ src/server/router/validate-data.js
@@ -19,7 +19,7 @@ module.exports = (obj) => {
     throw new Error(
       `Data must be an object. Found ${
         Array.isArray(obj) ? 'array' : typeof obj
-      }
+      }.
       'See https://github.com/typicode/json-server for example.`,
     )
   }
