e58a6ac4991e6312f5764ca30e3677f5cfd655f4
typicode <typicode@gmail.com>
2024-01-26 17:29:27 +0100

    chore: remove unused files

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./views/**/*.{html,js}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-}
