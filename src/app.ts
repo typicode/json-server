56969eaff35e22b7b5201cf148621810ced3d15c
nik-kita <nik-kita.deno.dev>
2024-03-08 00:59:58 +0100

    repair test for running in deno

--- src/app.ts
+++ src/app.ts
@@ -1,128 +1,138 @@
-import { dirname, isAbsolute, join } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { dirname, isAbsolute, join } from "node:path";
+import { fileURLToPath } from "node:url";
 
-import { App } from '@tinyhttp/app'
-import { cors } from '@tinyhttp/cors'
-import { Eta } from 'eta'
-import { Low } from 'lowdb'
-import { json } from 'milliparsec'
-import sirv from 'sirv'
+import { App } from "@tinyhttp/app";
+import { cors } from "@tinyhttp/cors";
+import { Eta } from "eta";
+import { Low } from "lowdb";
+import { json } from "milliparsec";
+import sirv from "sirv";
+import process from "node:process";
 
-import { Data, isItem, Service } from './service.js'
+import { Data, isItem, Service } from "./service.ts";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const isProduction = process.env['NODE_ENV'] === 'production'
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const isProduction = process.env["NODE_ENV"] === "production";
 
 export type AppOptions = {
-  logger?: boolean
-  static?: string[]
-}
+  logger?: boolean;
+  static?: string[];
+};
 
 const eta = new Eta({
-  views: join(__dirname, '../views'),
+  views: join(__dirname, "../views"),
   cache: isProduction,
-})
+});
 
 export function createApp(db: Low<Data>, options: AppOptions = {}) {
   // Create service
-  const service = new Service(db)
+  const service = new Service(db);
 
   // Create app
-  const app = new App()
+  const app = new App();
 
   // Static files
-  app.use(sirv('public', { dev: !isProduction }))
+  app.use(sirv("public", { dev: !isProduction }));
   options.static
     ?.map((path) => (isAbsolute(path) ? path : join(process.cwd(), path)))
-    .forEach((dir) => app.use(sirv(dir, { dev: !isProduction })))
+    .forEach((dir) => app.use(sirv(dir, { dev: !isProduction })));
 
   // CORS
-  app.use(cors()).options('*', cors())
+  app.use(cors()).options("*", cors());
 
   // Body parser
-  app.use(json())
+  app.use(json());
 
-  app.get('/', (_req, res) =>
-    res.send(eta.render('index.html', { data: db.data })),
-  )
+  app.get(
+    "/",
+    (_req, res) => res.send(eta.render("index.html", { data: db.data })),
+  );
 
-  app.get('/:name', (req, res, next) => {
-    const { name = '' } = req.params
-    const query = Object.fromEntries(Object.entries(req.query)
-      .map(([key, value]) => {
-        if (['_start', '_end', '_limit', '_page', '_per_page'].includes(key) && typeof value === 'string') {
-          return [key, parseInt(value)]
-        } else {
-          return [key, value]
-        }
-      })
-      .filter(([_, value]) => !Number.isNaN(value))
-    )
-    res.locals['data'] = service.find(name, query)
-    next()
-  })
+  app.get("/:name", (req, res, next) => {
+    const { name = "" } = req.params;
+    const query = Object.fromEntries(
+      Object.entries(req.query)
+        .map(([key, value]) => {
+          if (
+            ["_start", "_end", "_limit", "_page", "_per_page"].includes(key) &&
+            typeof value === "string"
+          ) {
+            return [key, parseInt(value)];
+          } else {
+            return [key, value];
+          }
+        })
+        .filter(([_, value]) => !Number.isNaN(value)),
+    );
+    res.locals["data"] = service.find(name, query);
+    next();
+  });
 
-  app.get('/:name/:id', (req, res, next) => {
-    const { name = '', id = '' } = req.params
-    res.locals['data'] = service.findById(name, id, req.query)
-    next()
-  })
+  app.get("/:name/:id", (req, res, next) => {
+    const { name = "", id = "" } = req.params;
+    res.locals["data"] = service.findById(name, id, req.query);
+    next();
+  });
 
-  app.post('/:name', async (req, res, next) => {
-    const { name = '' } = req.params
+  app.post("/:name", async (req, res, next) => {
+    const { name = "" } = req.params;
     if (isItem(req.body)) {
-      res.locals['data'] = await service.create(name, req.body)
+      res.locals["data"] = await service.create(name, req.body);
     }
-    next()
-  })
+    next();
+  });
 
-  app.put('/:name', async (req, res, next) => {
-    const { name = '' } = req.params
+  app.put("/:name", async (req, res, next) => {
+    const { name = "" } = req.params;
     if (isItem(req.body)) {
-      res.locals['data'] = await service.update(name, req.body)
+      res.locals["data"] = await service.update(name, req.body);
     }
-    next()
-  })
+    next();
+  });
 
-  app.put('/:name/:id', async (req, res, next) => {
-    const { name = '', id = '' } = req.params
+  app.put("/:name/:id", async (req, res, next) => {
+    const { name = "", id = "" } = req.params;
     if (isItem(req.body)) {
-      res.locals['data'] = await service.updateById(name, id, req.body)
+      res.locals["data"] = await service.updateById(name, id, req.body);
     }
-    next()
-  })
+    next();
+  });
 
-  app.patch('/:name', async (req, res, next) => {
-    const { name = '' } = req.params
+  app.patch("/:name", async (req, res, next) => {
+    const { name = "" } = req.params;
     if (isItem(req.body)) {
-      res.locals['data'] = await service.patch(name, req.body)
+      res.locals["data"] = await service.patch(name, req.body);
     }
-    next()
-  })
+    next();
+  });
 
-  app.patch('/:name/:id', async (req, res, next) => {
-    const { name = '', id = '' } = req.params
+  app.patch("/:name/:id", async (req, res, next) => {
+    const { name = "", id = "" } = req.params;
     if (isItem(req.body)) {
-      res.locals['data'] = await service.patchById(name, id, req.body)
+      res.locals["data"] = await service.patchById(name, id, req.body);
     }
-    next()
-  })
+    next();
+  });
 
-  app.delete('/:name/:id', async (req, res, next) => {
-    const { name = '', id = '' } = req.params
-    res.locals['data'] = await service.destroyById(name, id, req.query['dependent'])
-    next()
-  })
+  app.delete("/:name/:id", async (req, res, next) => {
+    const { name = "", id = "" } = req.params;
+    res.locals["data"] = await service.destroyById(
+      name,
+      id,
+      req.query["dependent"],
+    );
+    next();
+  });
 
-  app.use('/:name', (req, res) => {
-    const { data } = res.locals
+  app.use("/:name", (req, res) => {
+    const { data } = res.locals;
     if (data === undefined) {
-      res.sendStatus(404)
+      res.sendStatus(404);
     } else {
-      if (req.method === 'POST') res.status(201)
-      res.json(data)
+      if (req.method === "POST") res.status(201);
+      res.json(data);
     }
-  })
+  });
 
-  return app
+  return app;
 }
