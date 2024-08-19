161e01f5232db335afca6061515b4f542accd5c0
typicode <typicode@gmail.com>
2024-08-19 15:02:55 +0200

    chore: update linter

---
+++ .prettier.config.js
@@ -0,0 +1,5 @@
+export default {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+}
---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
