887cebb808b95b729d10a1e2b6d2fadf3ff42e71
MonkeyMike.eth <108690228+mon-key-mike@users.noreply.github.com>
2024-11-09 21:27:40 -0500

    Update wagmiConfig.ts

--- wagmiConfig.ts
+++ wagmiConfig.ts
@@ -1,1 +1,22 @@
+import { createConfig, configureChains, mainnet, sepolia, arbitrum } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { createStorage, cookieStorage } from 'wagmi';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
+export const { chains, publicClient } = configureChains(
+  [mainnet, sepolia, arbitrum],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Bridge App',
+  chains
+});
+
+export const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  chains,
+  publicClient,
+  storage: createStorage({ storage: cookieStorage })
+});
