cb0461ed96cf5c2e4f5c9f03f9e6f4f5ed0fc26c
MonkeyMike.eth <108690228+mon-key-mike@users.noreply.github.com>
2024-11-09 21:26:50 -0500

    Update app.tsx

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,1 +1,15 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { WagmiConfig } from 'wagmi';
+import { wagmiConfig, chains } from '../wagmiConfig';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
